12235c9f3946d8a40b5eeb1727f853cc36f67371
Gavamot <Gavamot@gmail.com>
2017-09-04 15:29:02 +0500

    hotfix

--- App.xaml.cs
+++ App.xaml.cs
@@ -8,6 +8,8 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Markup;
+using v1336.Model;
+using v1336.Rep.Dictionary;
 
 namespace v1336
 {
@@ -22,6 +24,10 @@ namespace v1336
             Thread.CurrentThread.CurrentUICulture = new CultureInfo("ru");
             FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement), new FrameworkPropertyMetadata(
                 XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
+
+            var rep = new WorkerPostRep();
+            if(rep.GetById(1) == null)
+                rep.Add(new WorkerPost(){Id = 1, Name = "Менеджер"});
         }
     }
 }
